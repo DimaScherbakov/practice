37019cf3f1d21e07e98bcf3e53427701e1bbc413
Dima Scherbakov <dima.r.scherbakov@gmail.com>
2019-02-06 12:01:50 +0200

    combineLatest usage example

--- src/app/a/a.component.ts
+++ src/app/a/a.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {CService} from "../c.service";
+import {combineLatest, fromEvent, Observable} from "rxjs";
 
 @Component({
   selector: 'app-a',
@@ -8,15 +9,26 @@ import {CService} from "../c.service";
 })
 export class AComponent implements OnInit {
   message: string;
+  click1Event$: Observable<any>;
+  click2Event$: Observable<any>;
   constructor(private service: CService) { }
 
   ngOnInit() {
-    this.service.emitterSource$.subscribe((data) => {
-      this.message = data;
+
+    this.click1Event$ = fromEvent(document.getElementById('btn1'),'click');
+    this.click2Event$ = fromEvent(document.getElementById('btn2'),'click');
+    let commonObserv$ = combineLatest(this.click1Event$,this.click2Event$);
+    commonObserv$.subscribe((data) => {
+      console.log(data);
+      this.message = 'both buttons have been clicked';
     })
-    setTimeout(() => {
-      this.service.changeMessage('new message');
-    },2000)
   }
 
+  click1(e){
+    this.message = 'button 1 has been clicked';
+  }
+
+  click2(e){
+    this.message = 'button 2 has been clicked';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AComponent } from './a/a.component';
-import { BComponent } from './b/b.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AComponent,
-    BComponent
+    AComponent
   ],
   imports: [
     BrowserModule
